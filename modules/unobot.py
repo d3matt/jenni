b925c82f664abfd15fe001c260221838c10d5804
Eli Carter <eli.carter@viavisolutions.com>
2017-02-09 14:39:26 -0600

    Fix display of play order and coloration at end of cards

--- modules/unobot.py
+++ modules/unobot.py
@@ -562,7 +562,7 @@ class UnoBot:
             return
         msg = STRINGS['NEXT_START']
 
-        future_play_order = self.playerOrder[self.currentPlayer+1:] + self.playerOrder[:self.currentPlayer]
+        future_play_order = self.playerOrder[self.currentPlayer+1:] + self.playerOrder[:self.currentPlayer+1]
         if self.way == -1:
             future_play_order.reverse()
         players_and_card_counts = [STRINGS['NEXT_PLAYER'] % (player, len(self.get_players_cards(player))) for player in future_play_order]
@@ -602,7 +602,7 @@ class UnoBot:
             else:
                 rendered = '%s(%s) [%s]' % (irc_colors[color], color, face)
             rendered_cards.append(rendered)
-        rendered_cards.append(irc_colors['*'])
+        rendered_cards.append(irc_colors[None])
         return ''.join(rendered_cards)
 
     def cardPlayable(self, card):
